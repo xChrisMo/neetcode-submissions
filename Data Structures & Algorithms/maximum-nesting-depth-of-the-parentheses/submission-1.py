abdd8af6c169ae550fc36da7196af7235a425665
Ayo Mosaku <mosakuayo@yahoo.com>
2026-05-31 00:43:26 +0100

    Add: maximum-nesting-depth-of-the-parentheses - submission-1

---
+++ Data Structures & Algorithms/maximum-nesting-depth-of-the-parentheses/submission-1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        max_n = 0
+        cur_n = 0
+
+        for char in s:
+            if char == '(':
+                cur_n += 1
+
+                max_n = max(max_n, cur_n)
+            
+            elif char == ')':
+                cur_n -= 1 
+
+
+        return max_n
