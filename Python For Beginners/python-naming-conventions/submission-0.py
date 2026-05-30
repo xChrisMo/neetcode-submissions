3fb575f648e8bb170d83943847e67990f6423e7d
Ayo Mosaku <mosakuayo@yahoo.com>
2026-05-30 18:57:16 +0100

    Add: python-naming-conventions - submission-0

---
+++ Python For Beginners/python-naming-conventions/submission-0.py
@@ -0,0 +1,7 @@
+pythonCreationDate = "February, 1991"
+javaCreationDate = "May, 1995"
+javascriptCreationDate = "December, 1995"
+
+print(pythonCreationDate)
+print(javaCreationDate)
+print(javascriptCreationDate)
