5c98bc821609fd6154423381ee175331d6a3f5ca
Ayo Mosaku <mosakuayo@yahoo.com>
2026-05-30 19:01:16 +0100

    Add: python-multiple-assignment - submission-0

---
+++ Python For Beginners/python-multiple-assignment/submission-0.py
@@ -0,0 +1,13 @@
+msg1, msg2 = "Hello", "World"
+msg3, msg4, msg5 = "My", "Name", "Is" 
+# Don't change the code above this line
+
+
+
+
+# Don't change the code below this line
+print(msg1)
+print(msg2)
+print(msg3)
+print(msg4)
+print(msg5)
