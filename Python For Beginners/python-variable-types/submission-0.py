1d8f1f4f89628ff6b01027dbd13c6c2a78d9beb5
Ayo Mosaku <mosakuayo@yahoo.com>
2026-05-31 21:11:12 +0100

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,11 @@
+integer_type = 20
+float_type = 30.5
+bool_type = True
+string_type = 'gg na goat'
+list_type = []
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
