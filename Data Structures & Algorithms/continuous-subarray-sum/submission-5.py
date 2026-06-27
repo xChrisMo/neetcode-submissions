61aab82aa9951d7ef21e07064962f5c21ecad098
Ayo Mosaku <mosakuayo@yahoo.com>
2026-06-28 00:03:35 +0100

    Add: continuous-subarray-sum - submission-5

---
+++ Data Structures & Algorithms/continuous-subarray-sum/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def checkSubarraySum(self, nums: List[int], k: int) -> bool:
+        #[23, 25, 29, 35, 42]
+        dict_nums = {0:-1} #if first is 0, we put -1. so i - - 1 would always be 
+        running_sum = 0
+
+        for i, num in enumerate(nums):
+            #increase running_sum
+            running_sum += num
+
+            #get reminder
+            val = running_sum % k
+
+            #chekc if reminder is stored
+            if val in dict_nums:
+                if i - dict_nums[val]> 1:
+                    return True
+
+            #store reminder with an index if not seen
+            else:
+                dict_nums[val] = i
+
+        return False
