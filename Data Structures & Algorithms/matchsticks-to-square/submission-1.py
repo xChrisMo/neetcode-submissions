336e21304897a0abd321948bfda50c078769a362
Ayo Mosaku <mosakuayo@yahoo.com>
2026-07-09 14:05:23 +0100

    Add: matchsticks-to-square - submission-1

---
+++ Data Structures & Algorithms/matchsticks-to-square/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def makesquare(self, matchsticks: List[int]) -> bool:
+        # one obvious.base case  
+        # it cant be a square if it isnt even a factor of 4
+        
+        if sum(matchsticks) % 4 != 0: return False
+
+        # so now we have to make an array of size 4, 
+        # where each element / subarray sums up to the side
+
+        length = sum(matchsticks) // 4
+
+        # sorting from largest to smallest lets us know if 1 element is larger than side, 
+        # if it is greater, return False already
+        out = []
+        matchsticks.sort(reverse=True)
+        sides = [0] * 4 
+
+        def subset(i):
+            # base condition
+            if i == len(matchsticks):
+                return True
+
+            # since we have for sides...
+            for j in range(4):
+                # if adding a matchstick i doesnt exceed the lenght, 
+                if sides[j] + matchsticks[i] <= length:
+                    # we add it and explore 
+                    sides[j] += matchsticks[i]
+                    if subset(i + 1) == True:
+                        return True
+                    sides[j] -= matchsticks[i]
+            return False
+
+        return subset(0)
