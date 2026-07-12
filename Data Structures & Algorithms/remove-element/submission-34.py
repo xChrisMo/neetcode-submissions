b79afc73621d123bd94aa5087a77bda22310f37c
Ayo Mosaku <mosakuayo@yahoo.com>
2026-07-12 20:31:46 +0100

    Add: remove-element - submission-34

---
+++ Data Structures & Algorithms/remove-element/submission-34.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        j = 0
+        n = len(nums)
+
+        for i in range(n):
+            if nums[i] != val:
+                nums[j] = nums[i]
+                j += 1
+
+        return j
