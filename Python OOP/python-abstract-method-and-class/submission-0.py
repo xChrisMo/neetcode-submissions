946d64b2315a098e823b59041efa7fb4f5876b4e
Ayo Mosaku <mosakuayo@yahoo.com>
2026-07-16 16:40:28 +0100

    Add: python-abstract-method-and-class - submission-0

---
+++ Python OOP/python-abstract-method-and-class/submission-0.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+class PaymentCard():
+    def __init__(self, card_number: str, balance: float):
+        self.card_number = card_number
+        self.balance = balance
+
+    # TODO: Implement the process_payment method
+    @abstractmethod
+    def process_payment(self, amount):
+        pass
+
+# TODO: Implement the DebitCard class
+
+class DebitCard(PaymentCard):
+    def process_payment(self, amount):
+        self.amount = amount
+        if self.balance > self.amount:
+            self.balance -= self.amount
+            return 'Payment successful'
+
+        else:
+            return 'Insufficient funds'
+
+# TODO: Implement the CreditCard class
+
+class CreditCard(PaymentCard):
+    def process_payment(self, amount):
+        self.amount = amount
+        self.balance -= self.amount
+        return 'Payment successful'
+
+
+# Don't modify the code below
+debit_card = DebitCard("1234", 100.0)  # Card with $100 balance
+credit_card = CreditCard("5678", 100.0) # Card with $100 balance
+
+# Test debit card
+print(debit_card.process_payment(50.0))  
+print(debit_card.balance)                
+print(debit_card.process_payment(100.0))  
+print(debit_card.balance)    
+
+# Test credit card
+print(credit_card.process_payment(50.0))   
+print(credit_card.balance)                
+print(credit_card.process_payment(100.0))  
+print(credit_card.balance)    
