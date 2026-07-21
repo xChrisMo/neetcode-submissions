679d62e14df43795ac0d4213100dbf12f50f1d8d
Ayo Mosaku <mosakuayo@yahoo.com>
2026-07-21 10:40:25 +0100

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.py
@@ -0,0 +1,23 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        def condition(m):
+            if nums[m] >= target:
+                return True
+
+        n = len(nums)
+        l = 0
+        r = n - 1
+
+        while l < r:
+            m = l + (r - l) // 2
+
+            if condition(m):
+                r = m
+
+            else:
+                l = m + 1
+
+        if nums[l] == target:
+            return l
+
+        return -1
