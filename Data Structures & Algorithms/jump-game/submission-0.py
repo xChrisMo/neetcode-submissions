e1aafb2a267a7f80005ba8eff2c6246a19e888c1
Ayo Mosaku <mosakuayo@yahoo.com>
2026-08-03 14:21:34 +0100

    Add: jump-game - submission-0

---
+++ Data Structures & Algorithms/jump-game/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        # for index, num in nums
+        # if sum >= last_index, return True
+        target = len(nums) - 1
+        max_reach = 0
+
+        for i, num in enumerate(nums):
+            if i > max_reach:
+                return False
+
+            if max_reach >= target:
+                return True
+            # / move forward
+            max_reach = max(max_reach, num + i)
+
+        return False
