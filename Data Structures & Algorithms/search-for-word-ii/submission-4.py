3d070c700e6c8d0770672799f5cc16c6e6816b64
Ayo Mosaku <mosakuayo@yahoo.com>
2026-08-06 17:14:11 +0100

    Add: search-for-word-ii - submission-4

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-4.py
@@ -0,0 +1,51 @@
+class TrieNode:
+    def __init__(self):
+        self.is_word = False
+        self.children = {}
+
+    def add_word(self, word):
+        curr = self
+
+        for char in word:
+            if char not in curr.children:
+                curr.children[char] = TrieNode()
+            curr = curr.children[char]
+        curr.is_word = True
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+        # basically is a trienode, but can insert...
+        for word in words:
+            root.add_word(word)
+
+        ROWS = len(board)
+        COLS = len(board[0])
+        seen = set()
+        res = set()
+
+        def dfs(r, c, node, word):
+            if r < 0 or r == ROWS or c < 0 or c == COLS or (r, c) in seen or board[r][c] not in node.children:
+                return 
+
+            # found a character in add
+            seen.add((r, c)) # add to a set
+            word += board[r][c] # build the word from '
+            node = node.children[board[r][c]] # moving into the word
+
+            if node.is_word:
+                res.add(word)
+
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            seen.remove((r, c))
+
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, '')
+
+        return list(res)
