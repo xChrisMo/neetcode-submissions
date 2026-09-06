243f68571d46cb94df48cbed7c929c5084d6ef89
Ayo Mosaku <mosakuayo@yahoo.com>
2026-09-06 23:31:02 +0100

    Add: interleaving-string - submission-10

---
+++ Data Structures & Algorithms/interleaving-string/submission-10.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        if len(s1) + len(s2) != len(s3):
+            return False
+
+        i = 0
+        j = 0
+
+        cache = {}
+
+        def dfs(i, j):
+            if (i, j) in cache:
+                return cache[(i, j)]
+
+            if i == len(s1) and j == len(s2):
+                cache[(i, j)] = True
+                return cache[(i, j)]
+                
+            ans = False
+
+            if i < len(s1) and s1[i] == s3[i + j]:
+                ans = ans or dfs(i + 1, j)
+
+            if j < len(s2) and s2[j] == s3[i + j]:
+                ans = ans or dfs(i, j + 1)
+
+            cache[(i, j)] = ans            
+            return cache[(i, j)]
+
+        return dfs(0, 0)
